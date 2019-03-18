96f7a2ff4ca63e9094f74ff0c2c7f3e6533abe52
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-18 15:09:33 +0530

    Create 100.c

---
+++ 100.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+   long int binary,hexa=0,i=1,remainder;
+   printf("Enter the binary number: ");
+   scanf("%ld",&binary);
+    while (binary!=0)
+    {
+      remainder=binary%10;
+     hexa= hexa+remainder*i;
+     i=i*2;
+     binary=binary/10;
+    }
+    printf("The hexadecimal value: %lX", hexa);
+    return 0;
+}
