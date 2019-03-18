3815b9bb0be385972f48dc2748af2e663f6a870e
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-18 15:02:56 +0530

    Create 93.c

---
+++ 93.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int n,a[50],i,t;
+  printf("Enter the value");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	if(n%2!=0)
+	{
+	for(i=0;i<n-1;i++)
+	{
+	    t=a[i];
+	    a[i]=a[i+1];
+	    a[i+1]=t;
+	    i++;
+	}
+	}
+	for(i=0;i<n;i++)
+	{
+	    printf("%d",a[i]);
+	}
+	return 0;
+}
