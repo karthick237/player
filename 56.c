7b888fa0782e938696f554710170af88ee263dfa
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-18 13:57:59 +0530

    Update 56.c

--- 56.c
+++ 56.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 int main(void) 
 {
-	char str[20],c;
-	int i;
-	scanf("%s %c",str,&c);
-	for(i=0;str[i]!='\0';i++)
-	{
-		if(str[i]==c)
+char str[20],c;
+int i;
+scanf("%s %c",str,&c);
+for(i=0;str[i]!='\0';i++)
+{
+	if(str[i]==c)
 		{
 			printf("%d",i+1);
 			break;
-		}
-	}
+		}}
 	return 0;
 }
