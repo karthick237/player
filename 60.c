12d29dd1638a6c339e908a2b650cf00dc44f5ce8
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-18 13:48:48 +0530

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+char s[10000],t[10000];
+ int i,c=0,l,l2;
+ gets(s);
+ gets(t);
+ l=strlen(t);
+ l2=strlen(s);
+ 
+ if(l==l2)
+ {
+ 
+ for(i=0;s[i]!='\0';i++)
+ {
+     if(s[i]==t[i])
+        c++;
+ }
+ 
+ 
+ if(c!=0)  
+   printf("yes");
+  else
+   printf("no");
+ }
+ else
+   printf("no");
+    return 0;
+}
