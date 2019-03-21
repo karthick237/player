f7d89c0b6f959ddefa333f23b645a6ebc2bfce10
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-21 16:37:09 +0530

    Create 106.c

---
+++ 106.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{ 
+    int a[100],i,n,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+  scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+    { 
+        if(a[i]==a[j])
+        {
+            a[i]='$';
+        }
+    }
+    }
+     for(i=0;i<n;i++)
+    {
+        if(a[i]!='$')
+        {
+        
+  printf("%d ",a[i]);
+    }
+    }
+ 
+    return 0;
+}
