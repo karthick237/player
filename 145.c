40ade87ff2b9422a5da8ad6aed6287632b18d24c
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-23 23:41:42 +0530

    Create 145.c

---
+++ 145.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int a[100],n,i;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(a[i]=(1-n))
+{
+printf("yes");
+break;
+}
+else
+{
+printf("no");
+break;
+}
+}
+getch();
+}
