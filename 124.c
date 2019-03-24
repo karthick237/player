3aabe2f04baa2e5dcf7fafba0e5801854a595be0
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-24 12:25:07 +0530

    Create 124.c

---
+++ 124.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void main()
+{
+
+  int n,a[100],i,j,c=0;
+  printf("Enter the value");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+for(i=2;;i++)
+{
+    c=0;
+    for(j=0;j<n;j++)
+    {
+        
+        if(i%a[j]==0)
+        {
+           c++;
+        }
+      
+    }
+    
+    if(c==n)
+    {
+    printf("%d",i);
+    break;
+    }
+}
+
+}
