f8ea7fa741010653e882d197e5b9dd1e10277254
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-24 13:50:02 +0530

    Create 117.c

---
+++ 117.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   char s[10000],a[10000];
+   int k=0,i,l;
+   gets(s);
+   l=strlen(s);
+   for(i=l-1;i>=0;i--)
+  {
+       a[k]=s[i];
+     k++;
+       }
+   for(i=0;a[i]!='\0';i++)
+  {
+          if(a[i+1]!='\0')
+        printf("%c-",a[i]);
+       else
+    printf("%c",a[i]);  
+  }
+ return 0;
+}
