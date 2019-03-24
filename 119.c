fc2c74d8a3f23a6e175afb4e13802698d027db35
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-24 13:45:45 +0530

    Create 119.c

---
+++ 119.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include<math.h>
+#include<string.h>
+int main()
+{
+    char hex[17];
+    long long decimal, place;
+    int i = 0, val, len;
+
+    decimal = 0;
+    place = 1;
+    gets(hex);
+    
+    len = strlen(hex);
+    len--;
+    len=strlen(hex);
+    len--;
+    for(i=0;hex[i]!='\0';i++)
+    {
+         if(hex[i]>='0' && hex[i]<='9')
+        {
+            val = hex[i] - 48;
+        }
+        else if(hex[i]>='a' && hex[i]<='f')
+        {
+            val = hex[i] - 97 + 10;
+        }
+        else if(hex[i]>='A' && hex[i]<='F')
+        {
+            val = hex[i] - 65 + 10;
+        }
+        decimal =decimal+ val * pow(16, len);
+        len--;
+       }
+    printf("%lld",decimal);
+    return 0;
+}
