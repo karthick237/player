1f9a8b407dceec6beae04dac836d8be794c885af
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-25 16:12:54 +0530

    Update 96.c

--- 96.c
+++ 96.c
@@ -3,18 +3,15 @@
 void main()
 {
   int i,rem,n,sum;
-  clrscr();
   scanf("%d",&n);
   rem=n%10;
-        n=n/10;
-        while(n>0)
-        {
-        i=n%10;
-        n=n/10;
-        }
-        sum=rem+i;
-        
-        printf("%d",sum);
-        
-    getch();
+  n=n/10;
+  while(n>0)
+ {
+  i=n%10;
+  n=n/10;
+ }
+ sum=rem+i;
+ printf("%d",sum);
+ getch();
 }
