d668677f6043bcb1d59a14e8b48eb451a98265b1
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-26 14:04:22 +0530

    Update 28.c

--- 28.c
+++ 28.c
@@ -1,1 +1,23 @@
-fhgfh
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int i,j,n,c=0,d,e=612;
+   char str[1000];
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+    {   d=0;
+        scanf("%s",&str[1000]);
+        for(j=0;str[j]!='\0';j++)
+        {
+         d=d+str[j];
+        }
+        if(d==e)
+        {
+         c++;
+        }
+    }
+    printf("%d",c);
+
+    return 0;
+}
