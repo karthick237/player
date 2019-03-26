3e3639b00f85ff558b649dd495634e9d99f49c5b
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-03-26 14:06:55 +0530

    Update 29.c

--- 29.c
+++ 29.c
@@ -1,3 +1,23 @@
-Input : 50
-Output : 
-2 5
+#include <stdio.h>
+int main(void) 
+{
+	int num,i,j,flag=1;
+  clrscr();
+	scanf("%d",&num);
+	for(j=2;j<=num;j++)
+	{
+		if(num%j==0)
+		{
+			flag=1;
+			for(i=2;i<=j/2;i++)
+			{
+			if(j%i==0)
+			{
+			flag=0;
+			break;}}
+      if(flag==1)
+	{
+		printf("%d ",j);
+	}}}
+	return 0;
+}
